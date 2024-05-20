e85d1d1eaa19a3fa7a2ac0821fd8f8937bc6cdb9
Yura-0 <yurec.name@gmail.com>
2024-05-20 13:53:02 +0300

    add assets

---
+++ lib/core/img_assets.dart
@@ -0,0 +1,5 @@
+abstract class ImgAssets {
+  static const String cardBack = 'assets/images/ui_block.png';
+  static const String backBtn = 'assets/images/ui_button_back.png';
+  static const String nextBtn = 'assets/images/ui_button_next.png';
+}
